4db80028ab7e97be967dab15f46314bf1abf9f2c
unicornmanager14 <unicorn0955464662>
2014-07-31 16:25:34 +0300

    Service

--- app/Controller/AboutController.php
+++ app/Controller/AboutController.php
@@ -8,9 +8,11 @@ class AboutController extends AppController{
 
     public function index(){
 
-
+        $data = array(
+            'page_title' => 'О нас'
+        );
         $this->layout = 'index';
-
+        $this->set($data);
     }
 
 }
